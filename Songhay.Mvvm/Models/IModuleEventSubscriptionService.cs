a84a05f2b46a97eafaf9358d5c41a0dbfbc0e781
Bryan Wilhite <bryan.wilhite@gmail.com>
2017-05-01 12:32:17 -0700

    added IModuleEventSubscriptionService

---
+++ Songhay.Mvvm/Models/IModuleEventSubscriptionService.cs
@@ -0,0 +1,19 @@
+﻿namespace Songhay.Mvvm.Models
+{
+    /// <summary>
+    /// Defines a wrapper for Module-level Prism events.
+    /// </summary>
+    /// <remarks>
+    /// Prism 6.x by default will destroy its <see cref="IModule"/> types
+    /// after they are initialized. It follows that event subscriptions
+    /// should not be there.
+    /// </remarks>
+    public interface IModuleEventSubscriptionService
+    {
+
+        /// <summary>
+        /// Sets up Prism eventing.
+        /// </summary>
+        void SetupPrismEventing();
+    }
+}
