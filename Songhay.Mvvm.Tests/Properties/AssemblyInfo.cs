1aae85f389cc6525bda9cff1c61320505cdf5d4c
Bryan Wilhite <rasx@songhaysystem.com>
2017-11-27 17:47:57 -0800

    :umbrella: :fire: test project will not compile without gen-web MVVM

---
+++ Songhay.Mvvm.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Songhay.Mvvm.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Songhay.Mvvm.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a69c810-5871-4c7e-bfc1-cd5a3fe45c18")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
